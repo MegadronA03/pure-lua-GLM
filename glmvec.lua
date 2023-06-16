190debf8fa34614e23697e005c5303f53800d964
MegadronA03 <megadron.b@gmail.com>
2023-06-17 04:09:11 +0500

    compressed __index, fixed swizzle variable set

--- glmvec.lua
+++ glmvec.lua
@@ -1,11 +1,17 @@
---TODO: error and type checking
 local vmt = {}
+local vpd = {}
 local errc = {
-	function (d) if type(d) ~= "number" then error("expected number or table with numbers",4) end end,
-	function (d,i) if #d>i then error("too many arguments",4) end end,
+	function (d) if type(d) ~= "number" then error("glmvec: expected number or table with numbers",2) end end,
+	function (d,i) if #d>i then error("glmvec: too many arguments",2) end end,
 }
 local errlib = function(c,...)
 	errc[c](...)
+end
+
+local loadvars = function(t,m)
+	for i,e in pairs(m) do
+		t[i]=e
+	end
 end
 
 local vec2 = function (x, y)
@@ -20,6 +26,7 @@ local vec2 = function (x, y)
 		errlib(1,y)
 		v = {x,y}
 	end
+	loadvars(v,vpd[1])
 	return setmetatable(v,vmt[1])
 end
 
@@ -56,7 +63,7 @@ end
 --				for i,e in pairs(it) do
 --					in_str = in_str..tostring(i)..": "..tostring(e).."\n"
 --				end
---				error("argument missmatch v:\n"..errstr.."\ninput:\n"..in_str,2)
+--				error("glmvec: argument missmatch v:\n"..errstr.."\ninput:\n"..in_str,2)
 --			end
 --		end
 --	end
@@ -97,13 +104,14 @@ local vec3 = function (...)
 				for i,e in pairs(it) do
 					in_str = in_str..tostring(i)..": "..tostring(e).."\n"
 				end
-				error("argument missmatch v:\n"..errstr.."\ninput:\n"..in_str,2)
+				error("glmvec: argument missmatch v:\n"..errstr.."\ninput:\n"..in_str,2)
 			end
 		end
 	end
 	if #v == 1 then
 		v = {v[1],v[1],v[1]}
 	end
+	loadvars(v,vpd[2])
 	return setmetatable(v,vmt[2])
 end
 
@@ -129,32 +137,27 @@ local vec4 = function (...)
 				for ke,va in pairs(it) do
 					in_str = in_str..tostring(ke)..": "..tostring(va).."\n"
 				end
-				error("argument missmatch v:\n"..errstr.."\ninput:\n"..in_str,2)
+				error("glmvec: argument missmatch v:\n"..errstr.."\ninput:\n"..in_str,2)
 		end
 	end
 	if #v == 1 then
 		v = {v[1],v[1],v[1],v[1]}
 	end
+	loadvars(v,vpd[3])
 	return setmetatable(v,vmt[3])
 end
 local lib = {vec2=vec2, vec3=vec3, vec4=vec4}
-local env = lib
-local vecconv = {tonumber, vec2, vec3, vec4}
+local env = {vec2=vec2, vec3=vec3, vec4=vec4, type=type, rawset=rawset, rawget=rawget, math=math}
+local function pass(x) return x end
 local ops = "+-*/"
 local ast = "xyzw"
+local cst = "rgba"
 
 local nmt = debug.getmetatable(0) or {}
 local mpn = {"__add","__sub","__mul","__div"}
 for a=1,4 do
 	local ac = ops:sub(a,a)
-	nmt[mpn[a]] = load(
-		[[return function(a,b)
-		if type(b) == 'table' then
-			for i,e in ipairs(b) do  --todo: number checking
-				b[i] = a]]..ac..[[e end
-		return b
-		else
-			return a]]..ac.."b\nend end"
+	nmt[mpn[a]] = load("return function(a,b)if type(b)=='table'then for i,e in ipairs(b) do b[i]=a"..ac.."e end return b else return a"..ac.."b end end"
 	)()
 end
 debug.setmetatable(0, nmt)
@@ -164,27 +167,33 @@ local vecgenmt = function (vt)
 	local vts = tostring(vt)
 	local tss = ""
 	local lss = ""
+	local iss = ""
 	for i=2,vt do
 		local s = tostring(i)
 		vc = vc..",a["..s.."] %s c["..s.."]"
 		tss = tss.."','..v["..s.."].."
 		lss = lss.."+v["..s.."]*v["..s.."]"
+		iss = iss..string.format(",%s=%s,%s=%s",ast:sub(i,i),s,cst:sub(i,i),s)
 	end
-	local fbs = string.format("return function (a, b)\nlocal c = vec%s(b)\nreturn vec%s(%s) \nend", vts,vts,vc)
+	local fbs = string.format("return function(a,b)local c = vec%s(b);return vec%s(%s)end", vts,vts,vc)
 	local fn_a = {}
 	for a=1,4 do
 		local ac = ops:sub(a,a)
 		fn_a[a] = load(string.format(fbs,ac,ac,ac,ac),"vec"..vts..":"..mpn[a],'t',env)()
 	end
-	local fn_ln = load("return function ( v ) return math.sqrt(v[1]*v[1]"..lss..") \nend","vec"..vts..":lenght",'t',env)()
-	local fn_i -- TODO: compress code
+	local fn_ln = load("return function(v)return math.sqrt(v[1]*v[1]"..lss..")end","vec"..vts..":lenght",'t',env)()
+	local fn_i = load("local vecconv={pass, vec2, vec3, vec4};return function(t,k)if type(k)=='string'then local vt={}if#k>4then error('glmvec: Invalid vector type'..tostring(#k),2)end local ds={x=1,r=1"..iss.."}for i=1,#k do local c=k:sub(i,i);vt[i]=rawget(t,ds[c])end return vecconv[#vt](vt)elseif type(k)=='number'then return rawget(t,k)else error('glmvec: invalid index type',2)end end"
+	,"vec"..vts..":__index",'t',env)()
+	local fn_ni = load("return function(t,k,v)if type(k)=='string'then if(#k<="..vts..")then local ds={x=1,r=1"..iss.."}if type(v)=='table'then if(#k==#v)then for i=1,#k do local c=k:sub(i,i);rawset(t,ds[c],v[i])end else error('glmvec: data length missmatch',2)end elseif type(v)=='number'then for i=1,#k do local c=k:sub(i,i);rawset(t,ds[c],v)end end else error('glmvec: invalid type',2)end elseif type(k)=='number'then rawset(t,k,v)else error('glmvec: invalid index type',2)end end"
+	,"vec"..vts..":__newindex",'t',env)()
 	return {
 		__add = fn_a[1],
 		__sub = fn_a[2],
 		__mul = fn_a[3],
 		__div = fn_a[4], --it doesnt have multiply optimization
-		__tostring = load("return function ( v ) return '<'..v[1].."..tss.."'>' end","vec"..vts..":tostring",'t',env)(),
+		__tostring = load("return function(v)return '<'..v[1].."..tss.."'>' end","vec"..vts..":tostring",'t',env)(),
 		__index = fn_i,
+		__newindex = fn_ni,
 		__pairs = function (v)
 			local i, value, key
 			return function()
@@ -197,6 +206,19 @@ local vecgenmt = function (vt)
 				end
 			end
 		end,
+		__ipairs = function (v)
+			local i = 0
+			return function()
+				i = i + 1
+				local value = v[i]
+				if value then
+					return i, value
+				else
+					return
+				end
+			end
+		end
+	}, {
 		_type = "vec"..vts,--compatability with Matrix.lua
 		dot = function (a, b)
 			local m = a*b
@@ -211,60 +233,9 @@ local vecgenmt = function (vt)
 end
 
 for i=1,3 do
-	vmt[i] = vecgenmt(i+1)
+	vmt[i], vpd[i] = vecgenmt(i+1)
 end
 
-vmt[2].cross = function (a, b) return vec3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x) end
+vpd[2].cross = function (a, b) return vec3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x) end
 
-vmt[1].__index = function (t, k)
-	if type(k) == "string" then
-		local vt = {}
-		if #k > 4 then error("Invalid vector type"..tostring(#k),2) end
-		for i = 1, #k do
-			local c = k:sub(i,i)
-			vt[i] = rawget(t,('x' == c) and 1 or 2)
-		end
-		
-		return vecconv[#vt](vt)
-	elseif type(k) == "number" then
-		return rawget(t,k)
-	else
-		error("invalid index type",2)
-	end
-end
-
-vmt[2].__index = function (t, k)
-	if type(k) == "string" then
-		local vt = {}
-		if #k > 4 then error("Invalid vector type"..tostring(#k),2) end
-		local ds = {x=1, y=2, z=3}
-		for i = 1, #k do
-			local c = k:sub(i,i)
-			vt[i] = rawget(t,ds[c])
-		end
-		return vecconv[#vt](vt)
-	elseif type(k) == "number" then
-		return rawget(t,k)
-	else
-		error("invalid index type",2)
-	end
-end
-
-vmt[3].__index = function (t, k)
-	if type(k) == "string" then
-		local vt = {}
-		if #k > 4 then error("Invalid vector type"..tostring(#k),2) end
-		local ds = {x=1, y=2, z=3, w=4}
-		for i = 1, #k do
-			local c = k:sub(i,i)
-			vt[i] = rawget(t,ds[c])
-		end
-		return vecconv[#vt](vt)
-	elseif type(k) == "number" then
-		return rawget(t,k)
-	else
-		error("invalid index type",2)
-	end
-end
-
-return lib
+return lib--TODO: error and type checking
